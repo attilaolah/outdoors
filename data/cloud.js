7e01ac9f955c283b29627d026eaf87d9b73e88cd
Attila Oláh <atl@google.com>
2020-07-03 16:45:05 +0200

    Check in data files.

---
+++ data/cloud.js
@@ -0,0 +1,43 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 875403,
+    "boundingBox": {
+        "lx": -14.432161331176758,
+        "ly": -11.373154640197754,
+        "lz": -10.644895553588868,
+        "ux": 10.307153701782227,
+        "uy": 13.36616039276123,
+        "uz": 14.094419479370118
+    },
+    "tightBoundingBox": {
+        "lx": -14.432161331176758,
+        "ly": -11.373154640197754,
+        "lz": -10.644895553588868,
+        "ux": 10.307153701782227,
+        "uy": 7.3258771896362309,
+        "uz": 3.3302249908447267
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.21424874663352967,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
