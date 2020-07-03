3fb6eb0973dd4a8f4ad91c9e9af4c2572eb91143
Attila Oláh <atl@google.com>
2020-07-03 16:45:05 +0200

    Check in data files.
    This branch contains "Kis u. 13", address:
    
        Mala 13
        24210 Bajmok
        Serbia

---
+++ models/kis-u-13/cloud.js
@@ -0,0 +1,43 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 7903618,
+    "boundingBox": {
+        "lx": -68.69259643554688,
+        "ly": -55.58972930908203,
+        "lz": -2.917745590209961,
+        "ux": 9.116537094116211,
+        "uy": 22.219404220581056,
+        "uz": 74.89138793945313
+    },
+    "tightBoundingBox": {
+        "lx": -68.69259643554688,
+        "ly": -55.58972930908203,
+        "lz": -2.917745590209961,
+        "ux": 3.4149723052978517,
+        "uy": 14.63357162475586,
+        "uz": 74.89138793945313
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.6738468408584595,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
